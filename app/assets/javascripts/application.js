cccf8beb9b5f462c9143b0fc57b9813a99b5dc77
Sarosh Arunkumar <sarosh.arunkumar@gmail.com>
2019-05-12 23:18:00 +0000

    new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-//= require jquery-ui
